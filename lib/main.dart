91f4f5fab80f0f82c7492051d295f9dd775855b9
LensaC <lensabez@outlook.com>
2024-03-11 22:58:23 +0530

    added recorder dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 
-      home:  SplashScreen(),
+      home:  VoiceRecognitionScreen(),
       //home: tour
       routes: {
         //  '/Settings' : (context) => FigmaToCodeApp(),
